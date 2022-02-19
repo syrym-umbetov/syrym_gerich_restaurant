f5d2bba277a796b419fa0521a8d7caaf45ac530c
syrym-umbetov <umbetov.syrym@gmail.com>
2022-02-19 22:40:53 +0600

    navbar fully done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
 import React from 'react';
 
-import { AboutUs, Chef, FindUs, Footer, Gallery, Header, Intro, Laurels, SpecialMenu } from './container';
+import {
+  AboutUs,
+  Chef,
+  FindUs,
+  Footer,
+  Gallery,
+  Header,
+  Intro,
+  Laurels,
+  SpecialMenu,
+} from './container';
 import { Navbar } from './components';
 import './App.css';
 
